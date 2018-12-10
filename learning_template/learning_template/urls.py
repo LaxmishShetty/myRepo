c4acf8cd2d945f365c3332a6763b80e9c4dda3b5
Prakash Shetty <l.prakash.shetty@accenture.com>
2018-12-10 11:28:28 +0530

    comment

--- learning_template/learning_template/urls.py
+++ learning_template/learning_template/urls.py
@@ -1,4 +1,4 @@
-"""learning_users URL Configuration
+"""learning_template URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,14 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url
 from django.contrib import admin
-from baisc_app import views
+from basic_app import views
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^admin/', admin.site.urls),
-    url(r'^baisc_app/',include('baisc_app.urls')),
-    url(r'^logout/',views.user_logout,name='user_logout'),
-    url(r'^special/',views.special,name='specail'),
+    url(r'^basic_app/',include('basic_app.urls')),
 ]
